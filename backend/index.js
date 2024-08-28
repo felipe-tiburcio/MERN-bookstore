0fb1932b1d3907a0aaec60ebe8c400253392e439
Felipe <felipe.tiburcio@gmail.com>
2024-08-27 21:53:10 -0300

    .env file with essential config data

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,22 @@
 import express from "express";
-import { PORT } from "./config.js";
+import dotenv from "dotenv/config";
+import mongoose from "mongoose";
 import { router } from "./routes/router.js";
 
 const app = express();
 
+const PORT = process.env.PORT;
+const mongoDbUrl = process.env.MONGO_DB_URL;
+
 app.use("/", router);
 
-app.listen(PORT, () => {
-  console.log(`Server running at port ${PORT}`);
-});
+mongoose
+  .connect(mongoDbUrl)
+  .then(() => {
+    console.log("Database connected!");
+
+    app.listen(PORT, () => {
+      console.log(`Server running at port ${PORT}`);
+    });
+  })
+  .catch((error) => console.log("Error in database connection: " + error));
