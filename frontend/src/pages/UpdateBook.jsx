5e32e6391b05138ed4a6e958962ccbcd088107ef
Felipe Tiburcio <felipe.tiburcio@gmail.com>
2024-09-03 15:31:25 -0300

    update book page

--- frontend/src/pages/UpdateBook.jsx
+++ frontend/src/pages/UpdateBook.jsx
@@ -1,7 +1,92 @@
+import { useEffect, useState } from "react";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+
 const UpdateBook = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishYear, setPublishYear] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+
+    axios
+      .get(`http://localhost:3000/books/${id}`)
+      .then((response) => {
+        setTitle(response.data.title);
+        setAuthor(response.data.author);
+        setPublishYear(response.data.publishYear);
+      })
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  const handleUpdateBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios
+      .put(`http://localhost:3000/books/${id}`, data)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => console.log(error))
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
-    <div>
-      <h1>Update Book</h1>
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Update Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+        <div className="my-4">
+          <label htmlFor="" className="text-xl mr-4 text-gray-500">
+            Title
+          </label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <div className="my-4">
+          <label htmlFor="" className="text-xl mr-4 text-gray-500">
+            Author
+          </label>
+          <input
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <div className="my-4">
+          <label htmlFor="" className="text-xl mr-4 text-gray-500">
+            Publish Year
+          </label>
+          <input
+            value={publishYear}
+            onChange={(e) => setPublishYear(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <button
+          className="p-2 bg-sky-300 m-8 rounded-md"
+          onClick={handleUpdateBook}
+        >
+          Update
+        </button>
+      </div>
     </div>
   );
 };
