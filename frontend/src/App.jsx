415beac2aa95900e435a38f80b3f6097a77c6f8d
Felipe Tiburcio <felipe.tiburcio@gmail.com>
2024-09-03 14:13:56 -0300

    book details page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,14 @@ import SaveBook from "./pages/SaveBook";
 import UpdateBook from "./pages/UpdateBook";
 import DeleteBook from "./pages/DeleteBook";
 import Home from "./pages/Home";
-import ShowBook from "./pages/ShowBook";
+import BookDetails from "./pages/BookDetails";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/books/save" element={<SaveBook />} />
-      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/books/details/:id" element={<BookDetails />} />
       <Route path="/books/update/:id" element={<UpdateBook />} />
       <Route path="/books/delete/:id" element={<DeleteBook />} />
     </Routes>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,4 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
-import axios from "axios";
 import Spinner from "../components/Spinner";
 import { Link } from "react-router-dom";
 import { AiOutlineEdit } from "react-icons/ai";
