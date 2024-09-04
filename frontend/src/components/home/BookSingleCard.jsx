21a18a0d426c890f328eacffe9d36c4c787dbdd3
Felipe <felipe.tiburcio@gmail.com>
2024-09-04 15:21:12 -0300

    book modal

--- frontend/src/components/home/BookSingleCard.jsx
+++ frontend/src/components/home/BookSingleCard.jsx
@@ -1,17 +1,18 @@
 /* eslint-disable react/prop-types */
 import { AiOutlineEdit } from "react-icons/ai";
-import { BiUserCircle } from "react-icons/bi";
+import { BiUserCircle, BiShow } from "react-icons/bi";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineDelete } from "react-icons/md";
 import { PiBookOpenTextLight } from "react-icons/pi";
 import { Link } from "react-router-dom/dist";
+import { useState } from "react";
+import BookModal from "./BookModal";
 
 const BookSingleCard = ({ book }) => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div
-      key={book._id}
-      className="border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl"
-    >
+    <div className="border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl">
       <h2 className="absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg">
         {book.publishYear}
       </h2>
@@ -25,16 +26,34 @@ const BookSingleCard = ({ book }) => {
         <h2 className="my-1">{book.author}</h2>
       </div>
       <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
+        <BiShow
+          className="text-3xl text-blue-500 hover:text-black cursor-pointer"
+          onClick={() => setShowModal(true)}
+          title="Summary"
+        />
+
         <Link to={`/books/details/${book._id}`}>
-          <BsInfoCircle className="text-2xl text-green-800 hover:text-black" />
+          <BsInfoCircle
+            className="text-2xl text-green-800 hover:text-black"
+            title="Details"
+          />
         </Link>
         <Link to={`/books/update/${book._id}`}>
-          <AiOutlineEdit className="text-2xl text-yellow-600 hover:text-black" />
+          <AiOutlineEdit
+            className="text-2xl text-yellow-600 hover:text-black"
+            title="Update"
+          />
         </Link>
         <Link to={`books/delete/${book._id}`}>
-          <MdOutlineDelete className="text-2xl text-red-600 hover:text-black" />
+          <MdOutlineDelete
+            className="text-2xl text-red-600 hover:text-black"
+            title="Delete"
+          />
         </Link>
       </div>
+      {showModal && (
+        <BookModal book={book} onClose={() => setShowModal(false)} />
+      )}
     </div>
   );
 };
